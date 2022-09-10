a3ddbbcfc36812d62f444af8d2668079825a75cf
Cxx <37257545+Cxx-mlr@users.noreply.github.com>
2022-09-09 23:49:11 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+function extendedAssignment(target, ...sources) {
+    sources.forEach(source => {
+        const descriptors = Object.keys(source).reduce((descriptors, key) => {
+            descriptors[key] = Object.getOwnPropertyDescriptor(source, key);
+            return descriptors;
+        }, {})
+
+        Object.getOwnPropertySymbols(source).forEach((symbol) => {
+            const descriptor = Object.getOwnPropertyDescriptor(source, symbol);
+            if (descriptor.enumerable) {
+                descriptors[symbol] = descriptor;
+            }
+        });
+
+        Object.defineProperties(target, descriptors);
+    });
+    return target;
+}
+
+const pair = {
+    get first() {
+        return 10;
+    },
+    get second() {
+        return -10;
+    }
+};
+
+console.log(Object.assign({}, pair)); // { first: 10, second: -10 }
+console.log(extendedAssignment({}, pair)); // { first: [Getter], second: [Getter] }
